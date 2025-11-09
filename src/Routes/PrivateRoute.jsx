38e7f797092ad4af84bfadcc69d4013e255c8e5a
Nazifah-The-G <nazifahanwar.09.09@gmail.com>
2025-11-10 01:55:53 +0600

    added private route

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+import React, { use } from "react";
+import { Navigate, useLocation } from "react-router";
+import Loader from "../Components/Loader";
+import { AuthContext } from "../AuthContext/AuthContext";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = use(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return <Loader/>;
+  }
+
+  if (user && user?.email) {
+    return children;
+  }
+  return <Navigate state={location.pathname} to="/login"></Navigate>;
+
+};
+
+export default PrivateRoute;
